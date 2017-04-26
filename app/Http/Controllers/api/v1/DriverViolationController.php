51e0128bc64d15a33453a7d269222bea0f4b1e43
jelojavier05 <jelo@Josephs-MacBook-Air.local>
2017-04-26 23:29:07 +0800

    conflict fixed

--- app/Http/Controllers/api/v1/DriverViolationController.php
+++ app/Http/Controllers/api/v1/DriverViolationController.php
@@ -55,13 +55,6 @@ class DriverViolationController extends Controller
                 ->select('strControlNumber')
                 ->orderBy('strControlNumber', 'desc')
                 ->first();
-<<<<<<< HEAD
-            if (is_null($result)){
-                $violationControlNumber = 'CO-000001-01';
-            } else {
-                $violationControlNumber = $result->strControlNumber;
-            }
-=======
 
             if (is_null($result)){
                 $violationControlNumber = 'CN-00001-01';
@@ -69,7 +62,6 @@ class DriverViolationController extends Controller
                 $violationControlNumber = $result->strControlNumber;
             }
 
->>>>>>> ubankAuth
             $counter = new SmartCounter();
 
             $id = DB::table('tblViolationTransactionHeader')->insertGetId([
